b48f5dd38842f2b5c0d9bceda5712cf99c4af4a0
andray-nkhatel <andreankhata@gmail.com>
2019-10-13 19:43:13 +0200

    added another button

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 let zero = 0;
 let data = document.querySelector('.data');
 let btn = document.querySelector('.btn');
+let btn2 = document.querySelector('.btn2');
 data.textContent= zero
 btn.onclick = () => {
             zero += 1;
+
+            data.textContent = zero;
+        }
+btn2.onclick = () => {
+            zero -= 1;
 
             data.textContent = zero;
         }
